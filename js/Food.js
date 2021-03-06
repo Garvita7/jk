841d5ee6284d6bfa86cc611abb454a29cd8183a7
Garvita7 <73775511+Garvita7@users.noreply.github.com>
2021-03-06 21:17:12 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,46 @@
+class Food{
+    constructor(){
+
+        this.image = loadImage("Milk.png");
+        this.foodStock = 0;
+
+    }
+
+    getFoodStock(){
+        var stateRef = database.ref('gameState');
+        stateRef.on("value",function(data){
+        gameState = data.val();
+       });
+    }
+
+    updateFoodStock(){
+        var state
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+
+    deductFood(){
+        if(mousePressed(feed)){
+            feedDog();
+         }
+        }
+    display(){
+      var x=80;
+      var y=100;
+
+      imageMode(CENTER);
+      image(this.image,720,220,70,70);
+
+      if(this.foodStock!=0){
+          for(var i=0;i<this.foodStock;i++){
+              if(i%10==0){
+                  x=80;
+                  y=y+50;
+              }
+              image(this.image,x,y,50,50);
+              x=x+30;
+          }
+      }
+    }
+}
